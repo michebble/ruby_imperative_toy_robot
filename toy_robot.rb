2d004c5dde3d65ae3d3c55ce869e07493ee6c5c8
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2021-05-15 11:12:03 +0900

    Add second command set
    While LEFT and MOVE commands are now hardcoded based on the command sets provided, PLACE has now been implemented to respect the provided details.
    However it does not care about the the board bounds.

--- toy_robot.rb
+++ toy_robot.rb
@@ -1,19 +1,26 @@
-# frozen_string_literal: true
+PLACE = "PLACE".freeze
+MOVE = "MOVE".freeze
+REPORT = "REPORT".freeze
+NORTH = "NORTH".freeze
+LEFT = "LEFT".freeze
 
-PLACE = "PLACE"
-MOVE = "MOVE"
-REPORT = "REPORT"
-NORTH = "NORTH"
+# commands = ["PLACE 0,0,NORTH", "MOVE", "REPORT"]
+commands = ["PLACE 0,0,NORTH", "LEFT", "REPORT"]
+current_commands = commands
 
-commands = ["PLACE 0,0,NORTH", "MOVE", "REPORT"].freeze
+x, y, facing = 0, 0, NORTH
 
-commands.each do |command|
-  x, y, facing = 0, 1, NORTH
-
+current_commands.each do |command|
   if command.start_with?(PLACE)
-    nil
+    command.delete_prefix!(PLACE)
+    command.strip!
+    new_x, new_y, facing = command.split(",")
+    x = new_x.to_i
+    y = new_y.to_i
   elsif command.eql?(MOVE)
-    nil
+    y += 1
+  elsif command.eql?(LEFT)
+    facing = "WEST"
   elsif command.eql?(REPORT)
     print [x, y, facing].join(",")
   end
